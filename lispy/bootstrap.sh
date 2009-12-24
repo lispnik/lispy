3bdf0f4854942f917c01c0e22aa06384e060dd10
mkennedy <mkennedy>
2009-12-24 18:01:24 +0000

    New dependencies.

--- lispy/bootstrap.sh
+++ lispy/bootstrap.sh
@@ -7,7 +7,7 @@ if [ -z "$BOOTSTRAP_VERSION" ]; then
     exit 1
 fi
 
-set -x
+set -ex
 
 SOURCES='http://common-lisp.net/project/lispy/repository/distfiles'
 
@@ -20,27 +20,33 @@ SOURCES='http://common-lisp.net/project/lispy/repository/distfiles'
 # 	      :key #'lispy:name))
 
 DEPENDENCIES='
+alexandria-2009-08-13.tar.gz
 archive_0.7.0.tar.gz
 asdf-1.108.tar.gz
-cffi_0.9.2.tar.gz
-chunga-0.4.1.tar.gz
-cl+ssl-2007-07-07.tar.gz
-cl-base64-3.3.2.tar.gz
-cl-fad-0.6.1.tar.gz
-drakma-0.11.1.tar.gz
-flexi-streams-0.13.1.tar.gz
+babel_0.3.0.tar.gz
+cffi_0.10.5.tar.gz
+chunga-1.1.0.tar.gz
+cl+ssl-2008-11-04.tar.gz
+cl-base64-3.3.3.tar.gz
+cl-fad-0.6.2.tar.gz
+cl-ppcre-2.0.1.tar.gz
+drakma-1.0.0.tar.gz
+flexi-streams-1.0.7.tar.gz
 gzip-stream_0.1.tar.gz
-ironclad_0.23.tar.gz
+ironclad_0.27.tar.gz
 lispy-0.5.tar.gz
+lispy-gpgme.tar.gz
+log5_0.3.1.tar.gz
 puri-1.5.1.tar.gz
 salza-0.7.4.tar.gz
 split-sequence-2002-04-10.tar.gz
-trivial-gray-streams-2006-09-16.tar.gz
-usocket-0.3.5.tar.gz
-log5_0.3.1.tar.gz'
+trivial-features_0.5.tar.gz
+trivial-gray-streams-2008-11-02.tar.gz
+trivial-shell_2008-09-01.tar.gz
+usocket-0.4.1.tar.gz'
 
 TMP=`mktemp -d -t bootstrap.sh.XXXXXX`
-trap "rm -rf $TMP* 2>/dev/null" EXIT
+# trap "rm -rf $TMP* 2>/dev/null" EXIT
 
 mkdir -p $TMP/lispy-all/distfiles
 
@@ -56,6 +62,7 @@ cat >$TMP/lispy-all/asdf-config.lisp <<EOF
        '(
 EOF
 find $TMP/lispy-all -mindepth 1 -maxdepth 1 -type d -not -name distfiles -print |sed -e "s,$TMP/lispy-all/,," | while read path; do
+    echo "Found $path"
     cat >>$TMP/lispy-all/asdf-config.lisp <<EOF
          #p"${path}/"
 EOF
